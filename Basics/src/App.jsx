4c235969442d10d13a2ed0814a3c1f21769a0f19
Kunal <kunalgupta0550@gmail.com>
2025-07-20 00:31:28 +0530

    counter project

--- Basics/src/App.jsx
+++ Basics/src/App.jsx
@@ -19,8 +19,12 @@ function App() {
     <>
       <h1>First Counter is : {counter}</h1>
       <h2>Second Counter is : {counter}</h2>
+      <h3>Third Counter is : {counter}</h3>
+      <h4>Fourth Counter is : {counter}</h4>
+      <h5>Fifth Counter is : {counter}</h5>
       <button onClick={increaseCounter}>Increase Count</button>
-      <button onClick={decreaseCounter}>Increase Count</button>
+      <br />
+      <button onClick={decreaseCounter}>Decrease Count</button>
     </>
   )
 }
