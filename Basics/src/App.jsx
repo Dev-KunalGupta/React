fac0054fd909135838b66fbbc88ce7fa8daa3b25
Kunal <kunalgupta0550@gmail.com>
2025-07-20 02:22:55 +0530

    interview question

--- Basics/src/App.jsx
+++ Basics/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 function App() {
-  let [counter, setCounter] = useState(15);
+  const [counter, setCounter] = useState(15);
 
   const increaseCounter = () => {
     if(counter !== 20){
@@ -14,6 +14,52 @@ function App() {
     setCounter(counter - 1);
     }
   }
+
+  /*
+  setCounter(callback) -> Hidden feature of setCounter i.e. it takes callback function.
+  Now, Interview Question ----------------------->
+  
+  const increaseCounter = () => {
+    if(counter !== 20){
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    }
+  }
+
+  const decreaseCounter = () => {
+    if(counter !== 0){
+    setCounter(counter - 1);
+    setCounter(counter - 1);
+    setCounter(counter - 1);
+    setCounter(counter - 1);
+    }
+  }
+
+  They all just increase or decrease only one why?
+  Bcoz useState jitne bhi updates bhejne hai UI ya variable mai batches mai bhejta hai isiliye now after fibre we got more control over it.
+  To do this have use the hidden feature of setCounter callback as callbacks are executed one by one.
+
+  const increaseCounter = () => {
+    if(counter !== 20){
+    setCounter(counter => counter + 1);
+    setCounter(counter => counter + 1);
+    setCounter(counter => counter + 1);
+    setCounter(counter => counter + 1);
+    }
+  }
+
+  const decreaseCounter = () => {
+    if(counter !== 0){
+    setCounter(prevCounter => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1); it's like ki yaha hum pehle previous state le rhe hai then update kar rhe.
+    }
+  }
+
+*/
 
   return (
     <>
