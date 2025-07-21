3ce9a1b1046c6c2ba564cf9cd923c9e53b1ee6cc
Kunal Gupta <154651173+Dev-KunalGupta@users.noreply.github.com>
2025-07-21 16:37:28 +0000

    done

--- basics/src/main.jsx
+++ basics/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <h1>I am Learning React</h1>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    App(),
+    MyApp()
 )
