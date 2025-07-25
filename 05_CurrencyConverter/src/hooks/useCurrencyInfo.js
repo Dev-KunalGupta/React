6d4524fbcb45226d5dd5de8435ce0f307cba1668
Kunal <kunalgupta0550@gmail.com>
2025-07-25 22:49:25 +0530

    make input component

--- 05_CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ 05_CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 
-function useCurrencyInfo(props){
+function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${props.currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then(res => res.json())
         .then(res => setData(res[currency]))
         console.log(data)
     },[currency])
+    return data
 }
 
-export default useCurrencyInfo()
+export default useCurrencyInfo;
