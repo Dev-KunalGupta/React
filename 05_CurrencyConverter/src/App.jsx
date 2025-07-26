385efbaf0c8e8443ea550b5acb56e8912ec22fd3
Kunal <kunalgupta0550@gmail.com>
2025-07-26 23:51:34 +0530

    app done

--- 05_CurrencyConverter/src/App.jsx
+++ 05_CurrencyConverter/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('https://www.pexels.com/photo/stock-report-on-black-surface-7947707/')`,
+        backgroundImage: `url('https://www.pexels.com/photo/numbers-on-monitor-534216/')`,
       }}
     >
       <div className="w-full">
@@ -40,14 +40,18 @@ function App() {
             <div className="w-full mb-1">
               <InputBox
                 label="From"
-
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setAmount(amount)}
+                selectCurrency={from}
+                onAmountChange={(amount) => setAmount(amount)}
               />
             </div>
             <div className="relative w-full h-0.5">
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-
+                onClick={swap}
               >
                 swap
               </button>
@@ -55,11 +59,15 @@ function App() {
             <div className="w-full mt-1 mb-4">
               <InputBox
                 label="To"
-
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={from}
+                amountDisable
               />
             </div>
             <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-              Convert
+              Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
           </form>
         </div>
