9aa3539f5cbd4f27929eded6788ae9d4fd3ed918
Kunal <kunalgupta0550@gmail.com>
2025-08-13 23:05:02 +0530

    complete Login and Profile component

--- 07_miniContext/src/App.jsx
+++ 07_miniContext/src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
     <UserContextProvider>
-      <h1>Hello, World!</h1>
+      <h1>This is the Webpage</h1>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
