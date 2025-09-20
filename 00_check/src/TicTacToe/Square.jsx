a9c2ae3b7d7dd310885b48aa23fedc76ace223b6
Kunal <kunalgupta0550@gmail.com>
2025-09-20 23:40:47 +0530

    make container

--- 00_check/src/TicTacToe/Square.jsx
+++ 00_check/src/TicTacToe/Square.jsx
@@ -1,6 +1,6 @@
 const Square = () => {
   return (
-    <div className="square">
+    <div style={{border: "1px solid", height: "100px", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}} className="square">
         <h2>X</h2>
     </div>
   )
